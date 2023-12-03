bfd2b70f16eb3f7e9801dfae67cb4a047cb01b3b
Muhaddil <151466679+Muhaddil@users.noreply.github.com>
2023-12-03 18:53:23 +0100

    Update 1

--- src/regions.ts
+++ src/regions.ts
@@ -2,13 +2,12 @@ export interface Regions {
 	[key: string]: string;
 }
 export const regions: Regions = {
-	'A21117FF': 'Flinus',
-	'A11117FF': 'Imnito',
-	'A11127FF': 'Hiuccli',
-	'A21127FF': 'Insesu',
-	'A21107FF': 'Cuynteto',
-	'A21107FE': 'Uskabar',
-	'A21117FE': 'Ziwananau',
+	'F7EC0D24': 'Bajíos de Uekenbe',
+	'032FE9B0': 'Bajíos de Uklots',
+	'FEA34C10': 'Frontera de Eighba',
+	'F7EC0D25': 'Xecroften',
+	'F8EBDD24': 'Areyas',
+	'0EEC7D10': 'Mar de Ticrops',
 }
 
 Object.freeze(regions);
