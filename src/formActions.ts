e738ccde5dd5953eb33273c9eba88b7d69e6e664
Lenni009 <guentherhanslphdyt@gmail.com>
2024-02-18 19:18:57 +0100

    use optional chaining

--- src/formActions.ts
+++ src/formActions.ts
@@ -3,7 +3,7 @@ import { showGlyphs } from './glyphLogic';
 import { isEisvanaSpace, setOutput } from './main';
 
 export function submit() {
-  if (!globalElements.input || !(globalElements.input.portalglyphsInput instanceof HTMLInputElement)) return;
+  if (!(globalElements.input?.portalglyphsInput instanceof HTMLInputElement)) return;
   const input = globalElements.input.portalglyphsInput.value.trim();
 
   const isEisvana = isEisvanaSpace(input);
@@ -14,7 +14,7 @@ export function submit() {
 
 // clears value of an input
 export function reset() {
-  if (!globalElements.input || !(globalElements.input.portalglyphsInput instanceof HTMLInputElement)) return;
+  if (!(globalElements.input?.portalglyphsInput instanceof HTMLInputElement)) return;
   globalElements.input.portalglyphsInput.value = '';
   showGlyphs();
   submit();
