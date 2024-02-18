3ddd57d34c6f181d559e24057ca1f1548ac46378
Lenni009 <guentherhanslphdyt@gmail.com>
2024-02-18 19:16:26 +0100

    wording

--- src/formActions.ts
+++ src/formActions.ts
@@ -2,18 +2,20 @@ import { globalElements } from './elementStore';
 import { showGlyphs } from './glyphLogic';
 import { isEisvanaSpace, setOutput } from './main';
 
-export function submit(): void {
-  const input = (globalElements.input!.portalglyphsInput as HTMLInputElement).value.trim();
+export function submit() {
+  if (!globalElements.input || !(globalElements.input.portalglyphsInput instanceof HTMLInputElement)) return;
+  const input = globalElements.input.portalglyphsInput.value.trim();
 
   const isEisvana = isEisvanaSpace(input);
-  const message = `You are ${isEisvana ? '' : 'not'} in Eisvana space!`;
+  const message = `You are ${isEisvana ? '' : 'not'} in Eisvana!`;
 
   setOutput(input ? message : '', isEisvana);
 }
 
 // clears value of an input
-export function reset(): void {
-  (globalElements.input!.portalglyphsInput as HTMLInputElement).value = '';
+export function reset() {
+  if (!globalElements.input || !(globalElements.input.portalglyphsInput instanceof HTMLInputElement)) return;
+  globalElements.input.portalglyphsInput.value = '';
   showGlyphs();
   submit();
 }
